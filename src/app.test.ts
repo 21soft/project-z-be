6dfa6926e6a100510f42ec28d89a20237cfffea7
nugrazurus <nugrazurus@student.untan.ac.id>
2023-10-04 08:52:01 +0700

    add test for register

--- src/app.test.ts
+++ src/app.test.ts
@@ -6,3 +6,23 @@ describe("/", () => {
     await supertest(app).get("/").expect("Content-Type", /json/).expect(200);
   });
 });
+
+describe("Register API", () => {
+  it("should return success if data is valid", async () => {
+    await supertest(app)
+      .post("/api/v1/register")
+      .send({
+        email: "fulan@mail.com",
+        password: "Password!23",
+      })
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+  it("should return bad request if data is not valid", async () => {
+    await supertest(app)
+      .post("/api/v1/register")
+      .send({})
+      .expect("Content-Type", /json/)
+      .expect(400);
+  });
+});
