080a15847007acbb2c5060621736b130b99465d6
nugrazurus <nugrazurus@student.untan.ac.id>
2023-10-04 08:55:54 +0700

    add register controller

--- src/controller/auth_controller.ts
+++ src/controller/auth_controller.ts
@@ -1,7 +1,46 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import { create } from "../repository/user_repository";
+import { genSaltSync, hashSync } from "bcryptjs";
+import { v4 } from "uuid";
+import { getErrorMessage } from "../util/error";
+
+const salt = genSaltSync(10);
 
 export const login = async (req: Request, res: Response) => {};
 
-export const register = async (req: Request, res: Response) => {};
+export const register = async (req: Request, res: Response) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: ReasonPhrases.BAD_REQUEST,
+        errors: errors.array(),
+      });
+
+    const { email, password } = req.body;
+
+    const user = await create({
+      uuid: v4(),
+      email: email,
+      password: hashSync(password, salt),
+    });
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: ReasonPhrases.OK,
+      data: user,
+    });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+        errors: getErrorMessage(error)
+    });
+  }
+};
 
 export const profile = async (req: Request, res: Response) => {};
--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { login, register } from "../controller/auth_controller";
+import { registerValidator } from "../validator/auth_validator";
 
 const router = Router()
 
 router.post('/login', login)
-router.post('/register', register)
+router.post('/register', registerValidator, register)
 router.get('/profile')
 
 export default router
