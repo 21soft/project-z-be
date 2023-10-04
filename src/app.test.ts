ef42f05290118b88dfa2494e146f4f0588d7ff48
nugrazurus <nugrazurus@student.untan.ac.id>
2023-10-04 11:43:00 +0700

    add more test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,7 @@
 import supertest from "supertest";
 import app from "./app";
+import {v4} from 'uuid'
+import {hashSync} from 'bcryptjs'
 
 describe("/", () => {
   it("GET should return 200", async () => {
@@ -7,22 +9,45 @@ describe("/", () => {
   });
 });
 
+
+jest.mock('./repository/user_repository', () => ({
+  create: jest.fn()
+}))
 describe("Register API", () => {
+
   it("should return success if data is valid", async () => {
-    await supertest(app)
+    jest.spyOn(require('./repository/user_repository'), 'create').mockReturnValue({
+      id: 1,
+      uuid: v4(),
+      email: "fulan@mail.com",
+      password: hashSync('Password!23', 10)
+    })
+
+    const res = await supertest(app)
       .post("/api/v1/register")
       .send({
         email: "fulan@mail.com",
         password: "Password!23",
       })
-      .expect("Content-Type", /json/)
-      .expect(200);
+    
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual({
+        success: true,
+        message: expect.any(String),
+        data: expect.any(Object)
+      })
   });
+
   it("should return bad request if data is not valid", async () => {
-    await supertest(app)
+    const res = await supertest(app)
       .post("/api/v1/register")
       .send({})
-      .expect("Content-Type", /json/)
-      .expect(400);
+
+    expect(res.statusCode).toEqual(400)
+    expect(res.body).toEqual({
+      success: false,
+      message: expect.any(String),
+      errors: expect.any(Object)
+    })
   });
 });
