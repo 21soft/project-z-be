f0a6db4a4e463ecbf30cb3a5311349b04db0b49e
nugrazurus <nugrazurus@student.untan.ac.id>
2023-10-05 23:12:39 +0700

    add login method and route

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { login, register } from "../controller/auth_controller";
-import { registerValidator } from "../validator/auth_validator";
+import { loginValidator, registerValidator } from "../validator/auth_validator";
 
 const router = Router();
 
@@ -9,6 +9,20 @@ const router = Router();
  * components:
  *   schemas:
  *     RegisterRequest:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: User's email address
+ *         password:
+ *           type: string
+ *           format: password
+ *           description: User's password
+ *     LoginRequest:
  *       type: object
  *       required:
  *         - email
@@ -73,7 +87,65 @@ const router = Router();
  *               errors: Registration failed
  */
 router.post("/register", registerValidator, register);
-router.post("/login", login);
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User login
+ *     description: User login with email and password.
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RegisterRequest'
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: true
+ *               message: OK
+ *               data:
+ *                 access_token: asdsad
+ *                 refresh_token: asdsad
+ *                 user:
+ *                  id: 12345
+ *                  uuid: 6bed2af1-066d-48d1-85fb-326703bed20c
+ *                  email: user@example.com
+ *                  displayName: user
+ *       400:
+ *         description: Bad request, validation errors
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: false
+ *               message: Bad Request
+ *               errors:
+ *                 - param: email
+ *                   msg: Invalid email
+ *                 - param: password
+ *                   msg: Password too short
+ *       404:
+ *         description: Not Found, User not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: false
+ *               message: Not Found
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: false
+ *               message: Internal Server Error
+ *               errors: Registration failed
+ */
+router.post("/login", loginValidator, login);
 router.get("/profile");
 
 export default router;
