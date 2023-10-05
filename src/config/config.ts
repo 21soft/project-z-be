a26e93a18350a8d5c1342f1da69dbd4abaeeae07
nugrazurus <nugrazurus@student.untan.ac.id>
2023-10-05 23:11:23 +0700

    add new env

--- src/config/config.ts
+++ src/config/config.ts
@@ -17,3 +17,8 @@ export const ServerConfig = {
     jwtSecret: process.env.JWT_SECRET,
     nodeEnv: process.env.NODE_ENV
 }
+
+export const JwtSecret = {
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET
+}
